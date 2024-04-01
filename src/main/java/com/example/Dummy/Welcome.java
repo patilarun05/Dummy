0af1b1fb23520e7151f03ac850c8233f18895ab5
patilarun05 <patilarun05@gmail.com>
2024-04-01 10:40:06 +0530

    new

---
+++ src/main/java/com/example/Dummy/Welcome.java
@@ -0,0 +1,10 @@
+package com.example.Dummy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+    @GetMapping("/Welcome")
+    public String getData() {return  "Welcome to DevOps Arun" ; }
+}
